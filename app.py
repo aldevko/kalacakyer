fc8af006935640d67f9abf03ae060f640307117a
AliDev <103180080+aldevko@users.noreply.github.com>
2023-02-11 23:55:32 +0300

    helpers func eklendi, arayuz duzeldi

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 
 app = Flask(__name__, static_folder="images")
 app.secret_key = 'kalaca'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/necati/Desktop/kalacak/kalacakyer.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/necati/Desktop/kalacak/kalacakyerlazim.db'
 db = SQLAlchemy(app)
 
 
@@ -21,8 +21,9 @@ class kalacakyer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30))
     countof = db.Column(db.Integer())
-    phonenum = db.Column(db.Integer(), unique=True)
+    phonenum = db.Column(db.Integer())
     location = db.Column(db.String(30))
+    emergency = db.Column(db.String(20))
     infonote = db.Column(db.String(240))
     date = db.Column(db.DateTime)
 
@@ -30,9 +31,19 @@ class kabulet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30))
     countof = db.Column(db.Integer())
-    phonenum = db.Column(db.Integer(), unique=True)
+    phonenum = db.Column(db.Integer())
     location = db.Column(db.String(30))
+    emergency = db.Column(db.String(20))
     infonote = db.Column(db.String(240))
+    date = db.Column(db.DateTime)
+
+class helperstable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+    kind = db.Column(db.Integer())
+    phonenum = db.Column(db.Integer())
+    infonote = db.Column(db.String(240))
+    location = db.Column(db.String(30))
     date = db.Column(db.DateTime)
 
 with app.app_context():
@@ -41,20 +52,39 @@ with app.app_context():
 @app.route("/", methods = ["GET","POST"])
 def index():
     username = request.form.get("username")
-    print(username)
     usercnt = request.form.get("usercount")
-    print(usercnt)
     userphone = request.form.get("usertel")
-    print(userphone)
     userlocation = request.form.get("userlocation")
-    print(userlocation)
     infonotte = request.form.get("usernote")
+    emergencyy = request.form.get("emergency")
+
+
+    helperuser = request.form.get("helpername")
+    helperkind = request.form.get("helperkind")
+    helpertele = request.form.get("helpertel")
+    helpernotee = request.form.get("helpernote")
+    helperloc = request.form.get("helperlocation")
+
+    if helperuser and helperkind and helpertele and helpernotee and helperloc:
+        newhelper = helperstable(
+                username = helperuser,
+                kind = helperkind,
+                phonenum = helpertele,
+                infonote = helpernotee,
+                location = helperloc,
+                date = datetime.datetime.now(),
+                )
+        db.session.add(newhelper)
+        db.session.commit()
+    
+
     if username and usercnt and userphone and userlocation:
         newuser = kalacakyer(
                 username = username,
                 countof = usercnt,
                 phonenum = userphone,
                 location = userlocation,
+                emergency = emergencyy,
                 infonote = infonotte,
                 date = datetime.datetime.now(),
                 )
@@ -66,24 +96,24 @@ def index():
 @app.route("/yerarayanlar", methods = ["GET","POST"])
 def yerarayanlar():
     import sqlite3 as sql
-    arayansorgu = sql.connect('kalacakyer.db')
+    arayansorgu = sql.connect('kalacakyerlazim.db')
     introcursor = arayansorgu.cursor()
     introcursor.row_factory = sql.Row
-    introappsel = introcursor.execute(f"SELECT * FROM kalacakyer").fetchall()
+    introappsels = introcursor.execute(f"SELECT * FROM kalacakyer ORDER BY location").fetchall()
     arayansorgu.commit()
     apprreq = request.form.get("approvereq")
 
     if apprreq:
-        apreeq = sql.connect('kalacakyer.db')
+        apreeq = sql.connect('kalacakyelazimr.db')
         introcursor = apreeq.cursor()
         introcursor.row_factory = sql.Row
         introappsel = introcursor.execute(f"SELECT * FROM kalacakyer WHERE id = {apprreq} ").fetchall()
         apreeq.commit()
-        #userid = introappsel[0]
         usernameapp = introappsel[0][1]
         usercount = introappsel[0][2]
         userphon = introappsel[0][3]
         userloc = introappsel[0][4]
+        emergent = '112 ihbarı Yapildi'
         usernote = introappsel[0][5]
 
         newappr = kabulet(
@@ -91,6 +121,7 @@ def yerarayanlar():
                 countof = usercount,
                 phonenum = userphon,
                 location = userloc,
+                emergency = emergent,
                 infonote = usernote,
                 date = datetime.datetime.now(),
                 )
@@ -108,7 +139,24 @@ def yerarayanlar():
         ttlcount.extend(i)
     countt = sum(map(int, ttlcount))
 
-    return render_template('yerarayanlar.html', introappsel = introappsel, countt = countt)
+    return render_template('yerarayanlar.html', introappsels = introappsels, countt = countt)
+
+@app.route("/helpers", methods = ["GET","POST"])
+def helpers():
+    import sqlite3 as sql
+    helpersorgu = sql.connect('kalacakyerlazim.db')
+    introcursor = helpersorgu.cursor()
+    introcursor.row_factory = sql.Row
+    helpers = introcursor.execute(f"SELECT * FROM helperstable ORDER BY kind").fetchall()
+    apprhelp = request.form.get("approvehelp")
+
+    if apprhelp:
+        introcursor.execute(f"DELETE FROM helperstable WHERE id = {apprhelp} ;")
+        helpersorgu.commit()
+        return redirect(url_for("helpers"))
+
+
+    return render_template('helpers.html', helpers = helpers)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
